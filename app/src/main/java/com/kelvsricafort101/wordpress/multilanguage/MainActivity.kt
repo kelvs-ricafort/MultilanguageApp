34bca3799228b0c958f885413e21fe35d87106a9
Kelvin Campos Ricafort <kelvs.r@outlook.com>
2026-09-25 15:51:25 +0800

    Add MultilanguageApp and update MainActivity

--- app/src/main/java/com/kelvsricafort101/wordpress/multilanguage/MainActivity.kt
+++ app/src/main/java/com/kelvsricafort101/wordpress/multilanguage/MainActivity.kt
@@ -4,10 +4,7 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
+import com.kelvsricafort101.wordpress.multilanguage.ui.MultilanguageApp
 import com.kelvsricafort101.wordpress.multilanguage.ui.theme.MultilanguageTheme
 
 class MainActivity : ComponentActivity() {
---
+++ app/src/main/java/com/kelvsricafort101/wordpress/multilanguage/ui/MultilanguageApp.kt
@@ -0,0 +1,8 @@
+package com.kelvsricafort101.wordpress.multilanguage.ui
+
+import androidx.compose.runtime.Composable
+
+@Composable
+fun MultilanguageApp() {
+    MultilanguageNavHost()
+}
